c6c495cea535202cc4a8e920904fac306478882d
Alex Paredes <alexparedesr95@gmail.com>
2022-03-08 15:53:11 -0600

    FEAT: Added state and listener to change mouse coords on mouse move in the HTML.

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
+    const { x, y } = coords;
+
     useEffect(() => {
+
+        const mouseMove = ( e ) => {
+            const coords = { x: e.x, y: e.y };
+            setCoords( coords );
+        };
       
-        console.log('Componente montado.');
+        window.addEventListener( 'mousemove', mouseMove );
     
         return () => {
-            console.log('Componente desmontado.');
+            window.removeEventListener( 'mousemove', mouseMove );
         }
     }, []);
     
@@ -15,6 +23,9 @@ export const Message = () => {
     return (
         <div>
             <h3>Eres genial!</h3>
+            <p>
+                x: { x } y: { y }
+            </p>
         </div>
     )
 }
