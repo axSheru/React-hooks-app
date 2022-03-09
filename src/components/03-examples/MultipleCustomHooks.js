20c0fe1494e458f8043dab97c451cbda98546b4d
Alex Paredes <alexparedesr95@gmail.com>
2022-03-08 22:41:03 -0600

    FEAT: Used counter hook to change the endpoint URL everytime is requested by the button and shows conditionally the component content.

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import { useFetch } from '../../hooks/useFetch';
+import { useCounterGenerico } from '../../hooks/useCounterGenerico';
 import '../../index.css';
 
 export const MultipleCustomHooks = () => {
 
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+    const { counter, increment } = useCounterGenerico(1);
 
-    console.log(state);
+    const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`);
+
+    const { author, quote } = !!data && data[0];
 
     return (
         <>
-            <h1>Custom Hooks!!!</h1>
+            <h1>Breaking Bad Quotes</h1>
+            <hr />
+
+            {
+                loading
+                ?
+                    (
+                        <div className='alert alert-info text-center'>Loading...</div>
+                    )
+                :
+                    (
+                        <>
+                            <blockquote className='blockquote text-right'>
+                                <p className='mb-0'>{ quote }</p>
+                                <footer className='blockquote-footer'>{ author }</footer>
+                            </blockquote>
+                            <button
+                                className='btn btn-outline-primary'
+                                onClick={ increment }
+                            >
+                                Siguiente quote
+                            </button>
+                        </>
+                    )
+            }
+            
         </>
     )
 }
