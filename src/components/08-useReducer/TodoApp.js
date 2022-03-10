484fde1b2dacf0fe1537174ad020568267a66063
Alex Paredes <alexparedesr95@gmail.com>
2022-03-10 15:53:45 -0600

    FEAT: Created HTML skeleton for TODO app.

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -16,14 +16,52 @@ export const TodoApp = () => {
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({ todos.length })</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className='list-group list-group-flush'>
+                        {
+                            todos.map( ( todo, i ) => (
+                                <li
+                                    key={ todo.id }
+                                    className='list-group-item'
+                                >
+                                    <p className='text-center'>{ i + 1 }. { todo.desc }</p>
+                                    <button
+                                        className='btn btn-outline-danger'
+                                    >
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className='form-control'
+                            placeholder='Debo de....'
+                            autoComplete='off'
+                        />
+                        <div className="d-grid gap-2">
+                            <button
+                                className='btn btn-outline-primary mt-1'
+                                type='button'
+                            >
+                                Agregar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
         </div>
     )
