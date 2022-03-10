3d8c462f47daa61d991304a1889108572c16dbfd
Alex Paredes <alexparedesr95@gmail.com>
2022-03-10 14:40:54 -0600

    FEAT: Created useReducer example with JS vanilla.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
@@ -12,9 +12,12 @@ import ReactDOM from 'react-dom';
 // import { Memorize } from './components/06-memos/Memorize';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
-import { Padre } from './components/07-tarea-memo/Padre';
+// import { Padre } from './components/07-tarea-memo/Padre';
 
 ReactDOM.render(
   <Padre />,
   document.getElementById('root')
 );
+ */
+
+import './components/08-useReducer/intro-reducer';
