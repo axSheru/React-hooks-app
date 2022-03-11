b4fd8e67c22876832a2626d2e7705cd6ae6de74c
Alex Paredes <alexparedesr95@gmail.com>
2022-03-11 15:20:33 -0600

    REFACTOR: Moved logic to add a new TODO to the TodoAdd component.

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
-import { useForm } from '../../hooks/useForm';
 import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 import './styles.css';
 
 const init = () => {
@@ -17,10 +17,6 @@ const init = () => {
 export const TodoApp = () => {
 
     const [ todos, dispatch ] = useReducer( todoReducer, [], init);
-
-    const [ { description }, handleInputChange, reset ] = useForm({
-        description: ''
-    });
 
     useEffect(() => {
         localStorage.setItem( 'todos', JSON.stringify( todos ) );
@@ -46,27 +42,13 @@ export const TodoApp = () => {
 
     };
 
-    const handleSubmit = ( e ) => {
-        e.preventDefault();
+    const handleAddTodo = ( newTodo ) => {
 
-        if ( description.trim().length <= 1 ) return;
-
-        console.log('Nueva tarea.');
-
-        const newTodo = {
-            id: new Date().getTime(),
-            desc: description,
-            done: false,
-        };
-
-        const action = {
+        dispatch({
             type: 'add',
             payload: newTodo,
-        };
+        });
 
-        dispatch( action );
-
-        reset();
     };
 
     return (
@@ -83,28 +65,9 @@ export const TodoApp = () => {
                     />
                 </div>
                 <div className="col-5">
-                    <h4>Agregar TODO</h4>
-                    <hr />
-
-                    <form onSubmit={ handleSubmit } >
-                        <input
-                            type="text"
-                            name="description"
-                            className='form-control'
-                            placeholder='Debo de....'
-                            autoComplete='off'
-                            value={ description }
-                            onChange={ handleInputChange }
-                        />
-                        <div className="d-grid gap-2">
-                            <button
-                                className='btn btn-outline-primary mt-1'
-                                type='submit'
-                            >
-                                Agregar
-                            </button>
-                        </div>
-                    </form>
+                    <TodoAdd
+                        handleAddTodo={ handleAddTodo }
+                    />
                 </div>
             </div>
 
