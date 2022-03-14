6bc1404c6127b935230553f7dd5fe6430dea1721
Alex Paredes <alexparedesr95@gmail.com>
2022-03-14 16:16:44 -0600

    FEAT: Imported UserContext and added ability to display the user in the userState and to reset it.

--- src/components/09-useContext/AboutScreen.js
+++ src/components/09-useContext/AboutScreen.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const AboutScreen = () => {
+
+    const { user, setUser } = useContext( UserContext );
+    const handleClick = () => {
+        setUser({});
+    };
+
     return (
         <div>
             <h1>AboutScreen</h1>
             <hr />
+            <pre className='container'>
+                {
+                    JSON.stringify( user, null, 3 )
+                }
+            </pre>
+            <button
+                className='btn btn-danger'
+                onClick={ handleClick }
+            >
+                logout
+            </button>
         </div>
     );
 }
