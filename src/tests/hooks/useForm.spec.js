51f252b93bfe8fbfbf44188c41cd7b347766bed6
Alex Paredes <alexparedesr95@gmail.com>
2022-03-14 22:33:19 -0600

    TEST: Added test to check form change in useForm hook.

--- src/tests/hooks/useForm.spec.js
+++ src/tests/hooks/useForm.spec.js
@@ -19,4 +19,24 @@ describe('Pruebas en el hook useForm', () => {
 
     });
 
+    test('debe de cambiar el valor del formulario (cambiar name).', () => {
+
+        const { result } = renderHook( () => useForm( initialForm ) );
+        const [ , handleInputChange ] = result.current;
+
+        act( () => {
+            handleInputChange({
+                target: {
+                    name: 'name',
+                    value: 'Mariana',
+                },
+            });
+        });
+
+        const [ formValues ] = result.current;
+
+        expect( formValues ).toEqual({ ...initialForm, name: 'Mariana' });
+
+    });
+
 });
