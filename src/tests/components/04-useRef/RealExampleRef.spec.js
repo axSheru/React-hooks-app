0b1a3bed5e24e81e9cb19846d3c80bd8bcbebaeb
Alex Paredes <alexparedesr95@gmail.com>
2022-03-15 17:06:58 -0600

    TEST: Added test to check component render after user click.

--- src/tests/components/04-useRef/RealExampleRef.spec.js
+++ src/tests/components/04-useRef/RealExampleRef.spec.js
@@ -3,11 +3,21 @@ import { RealExampleRef } from "../../../components/04-useRef/RealExampleRef";
 
 describe('Pruebas en el componente RealExampleRef.', () => {
 
+    const wrapper = shallow( <RealExampleRef /> );
+
     test('debe de hacer match con el snapshot.', () => {
 
-        const wrapper = shallow( <RealExampleRef /> );
-
         expect( wrapper ).toMatchSnapshot();
+
+        expect( wrapper.find( 'MultipleCustomHooks' ).exists() ).toBeFalsy();
+
+    });
+
+    test('debe de mostrar el componente MultipleCustomHooks', () => {
+
+        wrapper.find( 'button' ).simulate( 'click' );
+
+        expect( wrapper.find( 'MultipleCustomHooks' ).exists() ).toBeTruthy();
 
     });
 
