0bf11824072d610a551c31449eeacdb82bcf4992
Alex Paredes <alexparedesr95@gmail.com>
2022-03-15 16:08:33 -0600

    TEST: Updated snapshot test to use hook mock.

--- src/tests/components/03-examples/MultipleCustomHooks.spec.js
+++ src/tests/components/03-examples/MultipleCustomHooks.spec.js
@@ -1,13 +1,28 @@
 import { shallow } from "enzyme";
 import { MultipleCustomHooks } from "../../../components/03-examples/MultipleCustomHooks";
+import { useFetch } from "../../../hooks/useFetch";
+
+jest.mock( "../../../hooks/useFetch" );
 
 describe('Pruebas en el componente MultipleCustomHooks.', () => {
 
     test('debe de hacer match con el snapshot.', () => {
 
+        useFetch.mockReturnValue({
+            data: null,
+            loading: true,
+            error: null,
+        });
+
         const wrapper = shallow( <MultipleCustomHooks /> );
 
         expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('debe de mostrar la información.', () => {
+
+        useFetch
 
     });
 
